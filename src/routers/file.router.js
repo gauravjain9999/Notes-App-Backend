31eef34a1cd574ed5bb8b4876294aa42a818121d
gauravjain0926 <gauravjain0931@gmail.com>
2026-08-08 11:37:29 +0530

    Update File Routes and Services

--- src/routers/file.router.js
+++ src/routers/file.router.js
@@ -5,7 +5,6 @@ const verifyToken = require("../middleware/auth"); // ✅ auth middleware
 const upload = require("../middleware/upload-image");
 const fileUpload = require("../middleware/file-upload");
 const webClipController = require("../controller/webClip.controller");
-const documentsController = require("../controller/document.controller");
 router.use(verifyToken); // ✅ protect all file routes
 
 router.get("/files", verifyToken, fileController.file);
@@ -13,36 +12,16 @@ router.delete("/delete-file/:id", verifyToken, fileController.deleteFile);
 router.post("/upload", verifyToken, fileController.uploadFile);
 // Confirm upload & save final metadata
 router.post("/confirm-upload", verifyToken, fileController.confirmUpload);
-router.post(
-  "/upload-image",
-  verifyToken,
-  upload.single("image"),
-  fileController.uploadImage,
-);
 router.get("/images", verifyToken, fileController.getImages);
-router.delete("/delete-image/:id", verifyToken, fileController.deleteImage);
-
+router.get("/documents", verifyToken, fileController.getDocuments);
 // Web-Clips APIs
 router.post("/create-web-clip", verifyToken, webClipController.createWebClip);
+
 router.get("/web-clips", verifyToken, webClipController.getWebClips);
 router.delete(
   "/delete-web-clip/:id",
   verifyToken,
   webClipController.deleteWebClip,
-);
-
-router.post(
-  "/documents/upload",
-  verifyToken,
-  fileUpload.single("file"),
-  documentsController.uploadDocument,
-);
-router.get("/documents", verifyToken, documentsController.getDocuments);
-
-router.delete(
-  "/documents/delete/:id",
-  verifyToken,
-  documentsController.deleteDocument,
 );
 
 module.exports = router;
